e057083810b56aba9922d333facd7a440997059b
= <=>
2025-06-04 11:51:21 -0400

    inital

---
+++ README.md
@@ -0,0 +1,7 @@
+build for linux
+
+files will be in export folder
+
+copy to server
+
+run with argument --headless
---
+++ server.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var port
+
+func _ready():
+	var config = ConfigFile.new()
+	# Store some values.
+	config.set_value("app", "port", "1027")
+	# Save it to a file (overwrite if already exists).
+	config.save("user://easy_enet_config.cfg")
+	port = config.get_value("app", "port")
+	
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(int(port))
+	print("enet host is running on port: ", port)
+
+# may not need this - add in if needing to not overwrite config file each startup
+#func load_or_create_config():
+	#var config = ConfigFile.new()
+#
+	## Load data from a file.
+	#var err = config.load("user://easy_enet_config.cfg")
+#
+	## If the file didn't load, ignore it.
+	#if err != OK:
+		#return
+	#else:
+		#port = config.get_value("port")
